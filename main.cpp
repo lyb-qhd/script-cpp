dff9b1b2069d5d0dc2184c7f1e248a4c7fb6c353
lyb-qhd <linyubin8768@163.com>
2024-10-06 17:26:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define use using
+use uchar=unsigned char;
+use uint=unsigned int;
+use int64=long long;
+use uint64=unsigned long long;
+use llfloat=long double;
+use str=std::string;
+#undef use
+int main() {
+	#include "../script.cpp"
+	return 0;
+}
+
+
+
